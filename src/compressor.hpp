3b4e5a1afd8a9d06e67de89d7dad1bc107dd101e
Juan Alday <alday@ieee.org>
2024-11-29 18:26:24 -0500

    Initial push

---
+++ src/compressor.hpp
@@ -0,0 +1,13 @@
+#ifndef JAG_ZSTDPP_COMPRESSOR_HPP
+#define JAG_ZSTDPP_COMPRESSOR_HPP
+
+#include <zstd.h>
+
+namespace jag::compressor::zstd {
+
+	class Compressor {
+	};;
+
+} // end of namespace jag::compressor::zstd
+
+#endif // JAG_ZSTDPP_COMPRESSOR_HPP
---
+++ src/compressor.t.cpp
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+
+#include "compressor.hpp"
+
+
+TEST(Compressor, plain) {
+
+}
